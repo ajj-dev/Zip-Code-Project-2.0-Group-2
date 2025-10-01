65bbed5c2bfaac481dbf0a1231cc422b69919cb8
cjaawesome <69875824+cjaawesome@users.noreply.github.com>
2025-10-01 14:02:50 -0500

    HeaderBuffer.h Doccumentation

--- HeaderBuffer.h
+++ HeaderBuffer.h
@@ -7,18 +7,51 @@
 class HeaderBuffer 
 {
 public:
+    /**
+     * @brief Default constructor
+     * @details Initializes header buffer
+     */
     HeaderBuffer();
     
+    /**
+     * @brief read header
+     * @details Opens file, reads header into memory, interprets bytes, uses the header to guide how you read the rest of the file
+     * @param filename name of file being read
+     * @param header the header being read too
+     * @returns true or false depending on if the header was read successfully or not
+     */
     bool readHeader(const std::string& filename, HeaderRecord& header);
+    /**
+     * @brief read header
+     * @details Opens file, writes the header, and closes file
+     * @param filename name of file being written too
+     * @param header the header being written
+     * @returns true or false depending on if the header was write was successfully or not
+     */
     bool writeHeader(const std::string& filename, const HeaderRecord& header);
     
+    /**
+     * @brief has error
+     * @details returns buffer errorState
+     * @returns true or false depending on errorState
+     */
     bool hasError() const;
+
+    /**
+     * @brief error message
+     * @details returns buffer lastError
+     * @returns returns the last error message
+     */
     std::string getLastError() const;
     
 private:
     bool errorState;
     std::string lastError;
     
+    /**
+     * @brief set Error
+     * @details sets error state true a
+     */
     void setError(const std::string& message);
 };
 
